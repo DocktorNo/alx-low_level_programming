7835dd76b837551e100f1e859995ee5a80e285cd
DocktorNo <nyemitei@outlook.com>
2022-09-11 10:42:07 -0700

    print using write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	write(1, "and that piece of are is useful\" - Dora Korpar, 2015-10-19\n",59);
-	return(1);
+	write(1, "and that piece of is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
