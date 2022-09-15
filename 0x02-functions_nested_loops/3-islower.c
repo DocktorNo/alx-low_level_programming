80f9c233d314e64ba517f95b12c9cef9b6da6492
DocktorNo <nyemitei@outlook.com>
2022-09-15 01:27:57 -0700

    fixed betty error

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,8 @@
 
 /**
  * _islower -> checks if the letter is lowercase
- * 'c' is an integer 1 or 0
- * Returns: 1 if lowercase 0 if otherwiser
+ * @c: is an integer 1 or 0
+ * Return: 1 if lowercase 0 if otherwiser
  */
 int _islower(int c)
 {
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
 /**
  * _isalpha - checks for alphabetical letters
  * @c: a character to check for
- *Return: returns 0 if not a letter and returns 1 if a letter
+ * Return: returns 0 if not a letter and returns 1 if a letter
  */
 int _isalpha(int c)
 {
